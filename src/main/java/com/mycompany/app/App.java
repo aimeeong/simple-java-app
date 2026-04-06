4ad4ece5c218f807f4812b4ae83001d67036d310
Aimee Ong <aimee.ong@gmail.com>
2026-04-06 16:42:17 +0800

    My seventh change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Test1 Jenkins!");
+        System.out.println("Test2 Jenkins!");
     }
 
     public String getMessage() {
