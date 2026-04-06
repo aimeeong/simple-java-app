6d3b3de436a35003f682cf6b74a76ef3d5c3edd3
Aimee Ong <aimee.ong@gmail.com>
2026-04-06 15:58:23 +0800

    My fith change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Final Jenkins!");
+        System.out.println("Test Jenkins!");
     }
 
     public String getMessage() {
