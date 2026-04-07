cd35ec7ada08f818dcead94051a0051f663cc98e
Aimee Ong <aimee.ong@gmail.com>
2026-04-07 14:38:44 +0800

    My test3 commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Test2 Jenkins!");
+        System.out.println("Test3 Jenkins!");
     }
 
     public String getMessage() {
